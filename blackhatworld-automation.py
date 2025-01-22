45d8d77957145da752e09dfac193ce723bf9e55e
mitaliborad <mitaliia109@gmail.com>
2025-01-22 10:29:26 +0530

    add random,scroll

--- blackhatworld-automation.py
+++ blackhatworld-automation.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options 
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import random #import random module for generating random numbers.
 import time
 
 options = Options()
@@ -13,18 +16,21 @@ user_data = r"C:\Users\DELL\AppData\Local\Google\Chrome\User Data"  # profile pa
 profile_name = "Profile 37"  #profile name
 
 options.add_argument(f"user-data-dir={user_data}")  #instructs selenium to start chrome with user_data path.
-#options.add_argument(f"profile-directory={profile}")  #instructs selenium to tell chrome to use the Profile 37 in user_data path.
-
 driver = webdriver.Chrome(options=options)
-driver.get("https://www.blackhatworld.com/forums/content-copywriting.194/")
+driver.get("https://www.blackhatworld.com/")
 
-scroll_duration = 3  # shows total time spending on scrolling
-start_time = time.time() #records the current time when the scrolling starts.
+#Randomized Delays and Pauses:
+link = driver.find_element(By.LINK_TEXT, "Main Forum List")
+time.sleep(random.uniform(0.5, 2)) #Adding random pauses makes automated script appear more natural
+#random.uniform():a function from the random module used to generate a random floating-point number between two given values.
+link.click() #clicking on the element
 
-while(time.time() - start_time) < scroll_duration: # The while loop control the duration of scrolling
-#The current time will notice from the time scrolling starts which should not be more than 3 seconds
-    driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)# this line finds the body by tag name selector and pressing the END key to the page to scroll down
-    time.sleep(2) #makes the program wait for 2 seconds before continuing the loop.
+#Human-like Scrolling
+def scroll_down(driver):
+ for _ in range(13): # Scroll  times for this example
+  driver.execute_script("window.scrollBy(0,600);")
+  time.sleep(random.uniform(1.5,2))
+scroll_down(driver)
 
-time.sleep(3)
-driver.close()
+time.sleep(4)
+driver.quit()
